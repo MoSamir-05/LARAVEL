68e56c88d7a0851828d62ced858e7667a9c08d5b
MoSamir-05 <ps8949409@gmail.com>
2026-02-17 16:52:30 +0530

    17-02-26

--- project_8/routes/web.php
+++ project_8/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeCountroller;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::view('student/home','home1');
+// Route::get('student/show',[HomeCountroller::class,'show1']);
+// Route::get('student/show',[HomeCountroller::class,'add']);
+
+Route::prefix('student')->group(function(){
+    Route::view('/home','home1');
+    Route::get('/show',[HomeCountroller::class,'show1']);
+    Route::get('/add',[HomeCountroller::class,'add']);
+});
+
