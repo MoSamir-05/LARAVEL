b4a5293eb086156178f7b843b16fa8689ff4e391
MoSamir-05 <ps8949409@gmail.com>
2026-04-07 16:22:42 +0530

    07-04-26

--- project_18/app/Http/Controllers/Usercontroller.php
+++ project_18/app/Http/Controllers/Usercontroller.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 class Usercontroller extends Controller
 {
     function adduser(Request $request){
+        $request -> validate ([
+            'fname'=>'required |min:3 |max:10',
+            'lname'=>'required |min:3 |max:10',
+            'email'=>'required |email',
+            'password'=>'required |min:6',
+            'gender'=>'required',
+        ],[
+            'fname.required'=>'first name can not be empty',
+            'fname.min'=>'first name must be at least 3 characters',
+            'fname.max'=>'first name must be less than 10 characters',
+            'email.email'=>'email must be a valid'
+        ]);
         return $request;
     }
 }
--- project_18/resources/views/user-form1.blade.php
+++ project_18/resources/views/user-form1.blade.php
@@ -5,22 +5,39 @@
         Name <br>
         <input type="text" name="fname" placeholder="Enter your name">
         <br>
+        <span style="color:red">
+            @error('fname'){{ $message }}@enderror <br>
+        </span>
         Last Name <br>
         <input type="text" name="lname" placeholder="Enter your name">
         <br>
+        <span style="color:red">
+            @error('lname'){{ $message }}@enderror <br>
+        </span>
         email <br>
         <input type="email" name="email" placeholder="Enter your email">
         <br>
+        <span style="color:red">
+            @error('email'){{ $message }}@enderror <br>
+        </span>
+
         password <br>
         <input type="password" name="password" placeholder="Enter your password">
         <br>
+        <span style="color:red">
+            @error('password'){{ $message }}@enderror <br>
+        </span>
         Gender <br>
-        <input type="radio" id="male" name="gender" value="male">
+        <input type="radio" id="male" name="gender" value="male" {{ old('gender') == 'male' ? 'checked' : ''   }}>
         <label for="male">Male</label>
-        <input type="radio" id="female" name="gender" value="female">
-        <label for="female">Female</label>
-        <br><br>
 
+        <input type="radio" id="female" name="gender" value="female" {{ old('gender') == 'female' ? 'checked' : ''   }} >
+        <label for="female">Female</label>
+        <br>
+        <span style="color:red">
+            @error('gender'){{ $message }}@enderror <br>
+        </span>
+        <br>
         <input type="submit" value="Register">
     </form>
 </div>
